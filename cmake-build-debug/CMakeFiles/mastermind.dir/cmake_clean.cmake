6f1558cfed2971c1ba1b3c158c2477c35f385df2
Nadia <nadia.labbaci@gmail.com>
2018-04-18 01:34:32 -0400

    add lib skeleton

---
+++ cmake-build-debug/CMakeFiles/mastermind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mastermind.dir/source/library.cpp.o"
+  "libmastermind.pdb"
+  "libmastermind.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mastermind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/library.h
@@ -0,0 +1,6 @@
+#ifndef MASTERMIND_LIBRARY_H
+#define MASTERMIND_LIBRARY_H
+
+void hello();
+
+#endif
